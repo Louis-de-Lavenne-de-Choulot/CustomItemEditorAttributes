ef0db2296bfeadfca56e7db4460b0ff7fcac6fc8
Louis de Lavenne de Choulot <louisdelavennedechoulot@gmail.com>
2024-07-03 17:45:24 +0200

    nuget code

---
+++ net8.0/CustomItemEditorAttributes/CustomItemEditorAttributes/CustomItemEditorAttributes.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomItemEditorAttributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
+    public class CustomDescriptionAttribute : Attribute
+    {
+        public string Description { get; set; }
+        public CustomDescriptionAttribute(string description) => Description = description;
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DefaultStateAttribute : Attribute
+    {
+        public object DefaultState { get; set; }
+        public DefaultStateAttribute(object attrType) => DefaultState = attrType;
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CustomSelectionAttribute : Attribute
+    {
+        public string DefaultType { get; set; }
+        public CustomSelectionAttribute(string attrName) => DefaultType = attrName;
+    }
+}
